68e9863a941a63ff0db43cce4a852c73c6d3bacc
gxx <G000852@163.com>
2022-05-10 22:58:00 +0800

    test

--- 数据结构/src/LeetCode/只出现一次的数字_III.java
+++ 数据结构/src/LeetCode/只出现一次的数字_III.java
@@ -8,24 +8,24 @@ public class 只出现一次的数字_III {
         int[] nums = new int[]{-1, 0};
         int[] ints = solution260.singleNumber(nums);
         System.out.println(Arrays.toString(ints));
-
     }
 }
 
 
 class Solution260 {
     public int[] singleNumber(int[] nums) {
-        int ab = nums[0];
+        int a_b = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            ab = ab ^ nums[i];
+            a_b = a_b ^ nums[i];
         }
-        int right = ab & (~ab + 1); // 获取最右端的1
+        int One = a_b & (~a_b + 1); // 获取最右端的1
+//        int One = a_b & (-a_b); // 获取最左端的1
         int a = 0;
         for (int i = 0; i < nums.length; i++) {
-            if ((right & nums[i]) == 0) {
+            if ((One & nums[i]) == 0) {
                 a = a ^ nums[i];
             }
         }
-        return new int[]{a, a ^ ab};
+        return new int[]{a, a ^ a_b};
     }
 }
